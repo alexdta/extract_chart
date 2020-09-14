884964a0ce67a2c1d5f6b3d0d334cd1c725a3099
edgar <alexdta@gmail.com>
2020-09-14 14:00:38 -0600

    Primer commit

---
+++ extractChart/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace extractChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExtraer = new System.Windows.Forms.Button();
+            this.imgGrafico = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbGrafico = new System.Windows.Forms.ComboBox();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.lblHoja = new System.Windows.Forms.Label();
+            this.lblGraf = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGrafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExtraer
+            // 
+            this.btnExtraer.Location = new System.Drawing.Point(41, 12);
+            this.btnExtraer.Name = "btnExtraer";
+            this.btnExtraer.Size = new System.Drawing.Size(109, 32);
+            this.btnExtraer.TabIndex = 0;
+            this.btnExtraer.Text = "extraer grafs";
+            this.btnExtraer.UseVisualStyleBackColor = true;
+            this.btnExtraer.Click += new System.EventHandler(this.btnExtraer_Click);
+            // 
+            // imgGrafico
+            // 
+            this.imgGrafico.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgGrafico.Location = new System.Drawing.Point(41, 145);
+            this.imgGrafico.Name = "imgGrafico";
+            this.imgGrafico.Size = new System.Drawing.Size(709, 277);
+            this.imgGrafico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgGrafico.TabIndex = 1;
+            this.imgGrafico.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Grafico";
+            // 
+            // cmbGrafico
+            // 
+            this.cmbGrafico.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGrafico.Enabled = false;
+            this.cmbGrafico.FormattingEnabled = true;
+            this.cmbGrafico.Location = new System.Drawing.Point(41, 91);
+            this.cmbGrafico.Name = "cmbGrafico";
+            this.cmbGrafico.Size = new System.Drawing.Size(121, 24);
+            this.cmbGrafico.TabIndex = 4;
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Enabled = false;
+            this.btnExportar.Location = new System.Drawing.Point(621, 91);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(129, 32);
+            this.btnExportar.TabIndex = 6;
+            this.btnExportar.Text = "exportar a word";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // lblHoja
+            // 
+            this.lblHoja.AutoSize = true;
+            this.lblHoja.Location = new System.Drawing.Point(41, 125);
+            this.lblHoja.Name = "lblHoja";
+            this.lblHoja.Size = new System.Drawing.Size(37, 17);
+            this.lblHoja.TabIndex = 7;
+            this.lblHoja.Text = "Hoja";
+            // 
+            // lblGraf
+            // 
+            this.lblGraf.AutoSize = true;
+            this.lblGraf.Location = new System.Drawing.Point(108, 125);
+            this.lblGraf.Name = "lblGraf";
+            this.lblGraf.Size = new System.Drawing.Size(54, 17);
+            this.lblGraf.TabIndex = 8;
+            this.lblGraf.Text = "Grafico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGraf);
+            this.Controls.Add(this.lblHoja);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.imgGrafico);
+            this.Controls.Add(this.btnExtraer);
+            this.Controls.Add(this.cmbGrafico);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.imgGrafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExtraer;
+        private System.Windows.Forms.PictureBox imgGrafico;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbGrafico;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Label lblHoja;
+        private System.Windows.Forms.Label lblGraf;
+    }
+}
+
---
+++ extractChart/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace extractChart
+{
+    public partial class Form1 : Form
+    {
+
+        #region Variables
+
+        string DirectorioTemp;
+
+        #endregion
+
+        #region Constructor
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DirectorioTemp = Path.Combine(Environment.CurrentDirectory, "temp");
+
+            var x = 0;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        private void extraerimagen()
+        {
+            try
+            {
+                object lMissing = System.Reflection.Missing.Value;
+
+                var lExcelApp = new Excel.Application()
+                {
+                    Visible = true
+                };
+
+                string lArchivo = Environment.CurrentDirectory + "\\test.xlsx";
+
+                Excel.Workbook lLibro =
+                        lExcelApp.Workbooks.Open(lArchivo, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing, lMissing);
+
+                for (int i = 1; i <= lLibro.Worksheets.Count; i++)// i = num de hoja
+                {
+                    if (lLibro.Worksheets[i].ChartObjects().Count() > 0)
+                    {
+                        for (int j = 1; j <= lLibro.Worksheets[i].ChartObjects().Count(); j++)// j = numero de grafico
+                        {
+                            string lNombreGraf = $"grafico{i}-{j}";
+
+                            string lNombreArchivo = Path.Combine(DirectorioTemp, $"{lNombreGraf}.jpg");
+
+                            lLibro.Worksheets[i].ChartObjects(j).Activate();
+                            lLibro.Worksheets[i].ChartObjects(j).Chart.Export(lNombreArchivo, "JPG", false);
+
+                            cmbGrafico.Items.Add(lNombreGraf);
+                        }
+                    }
+                }
+
+                lLibro.Close();
+                lExcelApp.Quit();
+                lExcelApp = null;
+
+                if (cmbGrafico.Items.Count > 0)
+                {
+                    btnExportar.Enabled = true;
+                    cmbGrafico.Enabled = true;
+                    this.cmbGrafico.SelectedIndexChanged += new System.EventHandler(this.cmbGrafico_SelectedIndexChanged);
+                    cmbGrafico.SelectedIndex = 0;
+                }
+            }
+            catch (Exception lExcp)
+            {
+                throw new Exception("Error al extraer la imagen\n" + lExcp.Message);
+            }
+        }
+
+        private void insertarimagen(string pArchivoImagen)
+        {
+            try
+            {
+                object lMissing = System.Reflection.Missing.Value;
+
+                Word.Application lWordApp = new Word.Application()
+                {
+                    Visible = true
+                };
+
+                string lArchivo = Path.Combine(Environment.CurrentDirectory, "test.docx");
+
+                Word.Document lDocumento = lWordApp.Documents.Add(lArchivo);
+
+                object lBookMark = "marcador_imagen";
+                lDocumento.Bookmarks.get_Item(ref lBookMark).Range.InlineShapes.AddPicture(pArchivoImagen);
+
+                //lDocumento.PrintOut();
+
+                lWordApp = null;
+            }
+            catch (Exception lExcp)
+            {
+                throw new Exception("Error al insertar la imagen en el doc\n" + lExcp.Message);
+            }
+        }
+
+        #endregion
+
+        #region Botones
+
+        private void btnExtraer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(DirectorioTemp) == false)
+                {
+                    Directory.CreateDirectory(DirectorioTemp);
+                }
+
+                cmbGrafico.Items.Clear();
+                imgGrafico.Image = null;
+
+                extraerimagen();
+            }
+            catch (Exception lExcp)
+            {
+                MessageBox.Show(lExcp.Message);
+            }
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            string lArchivo = Path.Combine(DirectorioTemp, $"{cmbGrafico.Text}.jpg");
+            insertarimagen(lArchivo);
+        }
+
+        #endregion
+
+        #region Eventos
+
+        private void cmbGrafico_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string lArchivo = Path.Combine(DirectorioTemp,  $"{cmbGrafico.Text}.jpg");
+
+                string lExtract = cmbGrafico.Text.Replace("grafico", "");
+
+                lblHoja.Text = lExtract.Substring(0, lExtract.IndexOf('-'));
+
+                lblGraf.Text = lExtract.Substring(lExtract.IndexOf('-') + 1);
+
+                if (File.Exists(lArchivo) == true)
+                {
+                    Image img;
+                    using (var bmpTemp = new Bitmap(lArchivo))
+                    {
+                        img = new Bitmap(bmpTemp);
+                    }
+                    imgGrafico.Image = img;
+                }
+            }
+            catch (Exception lExcp)
+            {
+                MessageBox.Show(lExcp.Message);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Directory.Exists(DirectorioTemp) == true)
+            {
+                //var x = Directory.GetFiles(DirectorioTemp);
+
+                //foreach(string archivo in x)
+                //{
+                //    File.Delete(archivo);
+                //}
+
+                Directory.Delete(DirectorioTemp, true);
+            }
+
+        }
+        
+        #endregion
+
+    }
+}
